c4957aabaf857130c85af7d3b4a5d86208f2c92b
PoojaK97 <thisispooja97@gmail.com>
2020-01-21 19:00:15 +0530

    updated maps api key

--- Android/app/src/main/java/com/example/smartbus/TabThree.java
+++ Android/app/src/main/java/com/example/smartbus/TabThree.java
@@ -1,6 +1,7 @@
 package com.example.smartbus;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,12 +9,48 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class TabThree extends Fragment {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class TabThree extends Fragment implements OnMapReadyCallback {
+
+    SupportMapFragment mapFragment;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_three, container, false);
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment==null) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            mapFragment = SupportMapFragment.newInstance();
+            ft.replace(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
         return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap mMap) {
+
+        // Add a marker in Sydney and move the camera
+        LatLng MatthikereStop = new LatLng(13.1, 77.33);
+        Marker m = mMap.addMarker(new MarkerOptions().position(MatthikereStop).title("Matthikere Bus Stop"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(MatthikereStop));
+        mMap.setMaxZoomPreference(13f);
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 }
