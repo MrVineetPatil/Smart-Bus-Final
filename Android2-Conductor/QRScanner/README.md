e7d4f670c4f0d32eb933a9d60db81bbdc31f033c
PoojaK97 <thisispooja97@gmail.com>
2020-02-29 15:18:40 +0530

    presentation

---
+++ Android2-Conductor/QRScanner/README.md
@@ -0,0 +1,3 @@
+# QRScanner
+
+A basic android app to scan the QR made as a part of the Smart Bus Project for the conductors to verify Smart Payments.
---
+++ Android2-Conductor/QRScanner/app/src/main/java/com/project/insane/qrscanner/NotVerified.java
@@ -0,0 +1,16 @@
+package com.project.insane.qrscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotVerified extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_verified);
+
+
+    }
+}
